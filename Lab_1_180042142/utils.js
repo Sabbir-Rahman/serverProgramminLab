2750c261549fc5b4ca34dcc4e9bdbcc65766a6f4
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-04-30 00:32:00 +0600

    setTimeout and setInterval

--- Lab_1_180042142/utils.js
+++ Lab_1_180042142/utils.js
@@ -3,6 +3,13 @@
 //compiling means calling the whole file one time
 const HelloFunc = require("./helloWorld")
 
-HelloFunc.HelloFunction()
 
-console.log(HelloFunc.name)
+//1st parameter call back func and 2nd parameter time in milisecond
+setInterval(() => {
+    HelloFunc.HelloFunction()
+},1000)
+
+//call once a time after which time
+setTimeout(() => {
+    console.log(HelloFunc.name)
+},5000)
