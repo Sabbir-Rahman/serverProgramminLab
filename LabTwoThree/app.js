eb5b6aa5c36dc953b2a1a7013d0e5c017f2df6f0
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-06-17 06:32:58 +0600

    html load from template file

--- LabTwoThree/app.js
+++ LabTwoThree/app.js
@@ -4,13 +4,14 @@ const app = express()
 
 const PORT = 5000
 
+
 app.listen(PORT,(req,res)=>{
     console.log(`Server running on port ${PORT}`)
 })
 
 app.get('/',(req,res)=> {
 
-    res.send(`Hello from port ${PORT}`)
+    res.sendFile("login.html", { root: "./views/templates/AdminLTE-master/pages/examples" });
 })
 
 
