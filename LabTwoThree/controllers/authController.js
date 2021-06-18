1244da1b3db74ace548d7ece76e3625949bb1071
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-06-18 18:35:57 +0600

    user create and response issue fix

--- LabTwoThree/controllers/authController.js
+++ LabTwoThree/controllers/authController.js
@@ -1,29 +1,55 @@
 const userSchema = require('../model/userModel')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const alert = require('alert');
+var LocalStorage = require('node-localstorage').LocalStorage;
+const { request } = require('express');
+localStorage = new LocalStorage('./scratch');
+
 
 const getRegister = (req,res) => {
     res.sendFile("register.html", { root: "./views/templates/AdminLTE-master/pages/examples" })
+    
 }
 
 const postRegister = async (req,res) => {
     //fullname,email,pass,confpass
     const {fullname,email,password,confpassword} = req.body
 
-    const newUser = {fullname:fullname, email:email,password:password}
-    console.log(newUser)
+    if(password === confpassword){
 
-    try{
-        await new userSchema(newUser).save()
-        console.log('New User created')
-        res.status(201).json({newUser})
-    } catch (error){
-        res.status(409).json({message: error.message})
+        if(String(password).length <8){
+            res.status(400).json({message: 'Password must be atleast 8 charecters'})
+            
+        }else {
+            const hashPassword = bcrypt.hashSync(password, saltRounds);
+
+            const newUser = {fullname:fullname, email:email,password:hashPassword}
+
+            try{
+                await new userSchema(newUser).save()
+                localStorage.setItem('fullname',fullname)
+                alert("User Add Succesfully")
+                //res.status(201).json({message:'New user created', data:newUser}).redirect('/')
+            } catch (error){
+                res.status(409).json({message: error.message})
+            }
+    
+        }
+    }else{
+        res.status(400).json({message: 'Password and conf pass not match'})
     }
+    
+
     
 }
 
 
 const getLogin = (req,res) => {
     res.sendFile("login.html", { root: "./views/templates/AdminLTE-master/pages/examples" })
+    
+    //localStorage.removeItem('fullname')
+    console.log(localStorage.getItem('fullname'));
 }
 
 const postLogin = (req,res) => {
