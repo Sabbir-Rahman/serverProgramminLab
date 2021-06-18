2d5ed3e24225804545d26e5888961b2ca9e84502
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-06-18 09:56:57 +0600

    mongodb connected

--- LabTwoThree/app.js
+++ LabTwoThree/app.js
@@ -1,18 +1,27 @@
 const express = require('express')
-
 const app = express()
 
-const PORT = 5000
-
-app.use(express.static("public"))
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
 const authRoutes = require('./routers/authRoute')
+dotenv.config()
 
+const PORT = process.env.PORT
+const CONNECTION_URL = process.env.CONNECTION_URL
+app.use(express.static("public"))
 app.use(authRoutes)
 
-app.listen(PORT,(req,res)=>{
-    console.log(`Server running on port ${PORT}`)
-})
+
+
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Mongodb connected and Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false)
+
+
 
 
 
